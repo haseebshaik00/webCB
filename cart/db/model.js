ee6ccb132735866c33cf441b5f1e422c986e814a
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2022-02-22 01:10:21 +0530

    added models

--- cart/db/model.js
+++ cart/db/model.js
@@ -1,5 +1,35 @@
 const {Sequelize, db} = require('./connection');
 
 const User = db.define('users', {
-    
-})
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true 
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const Product = db.define('products', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true 
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    manufacturer: Sequelize.STRING,
+    price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0.0
+    }
+});
+
+module.exports = {
+    User, Product
+}
